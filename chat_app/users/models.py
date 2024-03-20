552da8d819be5d22480fea6958651589e021fe5c
Samir-Sattarov <samirsattarov2004@gmail.com>
2024-03-20 16:27:39 +0500

    Fixed models

--- chat_app/users/models.py
+++ chat_app/users/models.py
@@ -4,4 +4,10 @@ from django.db import models
 # Create your models here.
 class UserModel(models.Model):
     name = models.CharField(default="", max_length=255)
-    avatar = models.ImageField(default="", upload_to="uploads/")
+    surname = models.CharField(default="", max_length=255)
+    email = models.CharField(default="", max_length=255)
+    image = models.ImageField(default="", upload_to="uploads/")
+    phone = models.IntegerField(default=0)
+
+
+
